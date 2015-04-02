f74340e95880470890fe03d74228c7ff38976ec2
Frederik Elwert <frederik.elwert@web.de>
2015-04-02 20:59:07 +0200

    Update setup.py and bump version to 0.2.

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from distutils.core import setup
 from glob import glob
 
 setup(name='TCFnetworks',
-      version='0.1',
+      version='0.2',
       description='Python TCF network services',
       author='Frederik Elwert',
       author_email='frederik.elwert@web.de',
-#      url='',
-      packages=['tcfnetworks'],
-#      package_data={'tcfnetworks.exporters': glob('data/*.xsl')},
+      url='https://github.com/SeNeReKo/TCFnetworks',
+      packages=['tcfnetworks', 'tcfnetworks.annotators',
+      		    'tcfnetworks.exporters'],
+      package_data={'tcfnetworks.exporters': glob('data/*.xsl')},
      )
